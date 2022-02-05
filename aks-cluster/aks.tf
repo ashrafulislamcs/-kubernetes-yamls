0338ace7cf6fd53f201abcc2f18bdafb12d806ba
Ashadullah Shawon <shawonashadullah@gmail.com>
2022-02-05 17:55:59 +0600

    Create aks.tf

---
+++ aks-cluster/aks.tf
@@ -0,0 +1,23 @@
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "stage-aks-cluster"
+  location            = var.location
+  resource_group_name = var.azure_resource_group
+  dns_prefix          = "dns-k8s"
+
+  default_node_pool {
+    name                = "stage"
+    node_count          = 1
+    vm_size             = "Standard_B2s"
+    os_disk_size_gb     = 40
+    enable_auto_scaling = false
+    vnet_subnet_id      = azurerm_subnet.subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "stage"
+  }
+}
